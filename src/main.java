cf1cf1427ba3bfa60df48db1eb71db9fbfc6b318
倪银龙 <niyinlong@126.com>
2019-08-03 10:53:47 +0800

    Hello world

---
+++ src/main.java
@@ -0,0 +1,10 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		phone xiaomi = new phone();	
+		System.out.println("hello");
+		xiaomi.call();
+	}
+
+}
---
+++ src/phone.java
@@ -0,0 +1,15 @@
+
+public class phone {
+	// 属性
+	float sreen;
+	float cpu;
+	float mem;
+	// 方法
+	void call() {
+		System.out.print("打电话");
+	}
+	
+	void sendMsg() {
+		System.out.print("发短信");
+	}
+}
